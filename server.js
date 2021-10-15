0acb69fcb081d559284a96426cd6fe9abfe96f14
Your Name <you@example.com>
2021-10-14 19:47:32 -0800

    updating database

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const image = require('./controller/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-colorful-051911',
     port : 5432,
     user : 'postgres',
     password : 'jeddy019',
