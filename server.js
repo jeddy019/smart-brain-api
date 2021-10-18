14dbb59229af0f3a59cf34df06cd2de5c7288d52
Your Name <you@example.com>
2021-10-17 17:08:36 -0800

    fixed

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
 const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
-const knex = require('knex');
+const db = require('knex')({
+  client: 'pg',
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+  }
+});
 
 const register = require('./controller/register');
 const signin = require('./controller/signin');
 const profile = require('./controller/profile');
 const image = require('./controller/image');
 
-const db = knex({
-  client: 'pg',
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
 
 const app = express();
+const PORT= process.env.PORT ? process.env.PORT : 3000;
 
 app.use(express.json());
 app.use(cors());
 
 
-app.get('/', (req, res) => { res.send('it is working!') })
+app.get('/', (req, res) => { res.send('WELCOME!') })
 
 app.post('/signin', signin.handleSignin(db, bcrypt))
 
@@ -34,6 +34,6 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 
 app.post('/imageUrl', (req, res) => { image.handleApiCall(req, res) })
 
-app.listen(process.env.PORT || 3000,()=> {
-	console.log(`app is running on port ${process.env.PORT}`);
+app.listen(PORT, ()=> {
+	console.log(`app is running on port ${PORT}`);
 });
