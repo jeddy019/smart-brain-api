13ef579b0bff203286c79392e3041e5adcfe0e7a
Your Name <you@example.com>
2021-10-23 13:10:39 -0800

    finally done!

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
-const db = require('knex')({
-  client: 'pg',
-  connection: {
-    connectionString: process.env.DATABASE_URL,
-    ssl: true,
-  }
-});
+const knex = require('knex');
 
 const register = require('./controller/register');
 const signin = require('./controller/signin');
 const profile = require('./controller/profile');
 const image = require('./controller/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+  }
+});
 
 const app = express();
 const PORT = process.env.PORT ? process.env.PORT : 3000;
